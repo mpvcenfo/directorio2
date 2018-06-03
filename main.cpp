527a0da0159141cde0585dd80745c2c7a0be580b
Marco Piedra Venegas <mpiedrav@gmail.com>
2018-06-02 23:49:20 -0600

    Initial

---
+++ main.cpp
@@ -0,0 +1,7 @@
+// Universidad Cenfotec
+// Estructura de datos 1 - 2018-01
+// Nombre de archivo: main.cpp
+// Autor: Marco Piedra Venegas (mpiedrav@ucenfotec.ac.cr)
+// TAD directorio - Tarea 2
+// Fecha de creación: 02-06-2018
+// Fecha de modificación: 02-06-2018
